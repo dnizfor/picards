b73febb9c77a7867f700efe93ebabe0049102640
dniz <dnizfor@gmail.com>
2023-07-22 11:02:27 +0300

    develop draft set list screen

--- src/screens/SetListScreen.js
+++ src/screens/SetListScreen.js
@@ -1,8 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlusButton from "../components/global/PlusButton";
 import ChooseMenu from "../components/SetListScreen/ChooseMenu";
 import { useState } from "react";
+import SetCard from "../components/getCardScreen/SetCard";
+import { FlatList } from "react-native";
 
 export default function SetListScreen({ navigation }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,11 +24,27 @@ export default function SetListScreen({ navigation }) {
       },
     },
   ];
+
+  const renderItems = ({ item }) => (
+    <SetCard title={item.set_name} count={"18-word"} />
+  );
+  const data = [{ set_name: "set_name", set_id: 1 }];
   return (
     <SafeAreaView style={setListCreenContainer.container}>
       <View style={setListCreenContainer.plusButtonContainer}>
         <PlusButton onPress={() => setIsOpen(true)} />
       </View>
+      <TextInput
+        style={setListCreenContainer.inputContainer}
+        // onChangeText={onChangeText}
+        // value={text}
+        placeholder="Find Set"
+      />
+      <FlatList
+        data={data}
+        renderItem={renderItems}
+        showsVerticalScrollIndicator={false}
+      />
       {isOpen && (
         <ChooseMenu
           onPressToBackground={() => setIsOpen(false)}
@@ -42,10 +60,22 @@ const setListCreenContainer = StyleSheet.create({
     flex: 1,
     backgroundColor: "yellow",
     position: "relative",
+    alignItems: "center",
   },
   plusButtonContainer: {
     position: "absolute",
     bottom: 20,
     right: 20,
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    width: 300,
+    height: 50,
+    borderRadius: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 15,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
 });
