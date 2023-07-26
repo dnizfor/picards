a3e2a7760fbce9c101d07b42fb9f60eeee1f4fb0
dniz <dnizfor@gmail.com>
2023-07-26 15:50:35 +0300

    add sliding feature to feed screen

--- src/screens/FeedSreen.js
+++ src/screens/FeedSreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function FeedSreen() {
+  const array = [1, 2, 3];
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={feedScreenStyle.postContainer}>
+        <Text>{item}</Text>
+      </View>
+    );
+  };
   return (
     <SafeAreaView>
-      <Text>FeedSreen</Text>
+      <FlatList
+        renderItem={renderItem}
+        data={array}
+        pagingEnabled={true}
+        keyExtractor={(item) => item}
+        decelerationRate={"normal"}
+      />
     </SafeAreaView>
   );
 }
+const feedScreenStyle = StyleSheet.create({
+  postContainer: {
+    flex: 1,
+    height: Dimensions.get("screen").height - 160,
+    backgroundColor: "yellow",
+  },
+});
