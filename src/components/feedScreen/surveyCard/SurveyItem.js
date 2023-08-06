9d09879da5afe6d2069f8da05ba40b9fc0704a1b
dniz <dnizfor@gmail.com>
2023-08-06 20:04:01 +0300

    create StyleSheet Property

--- src/components/feedScreen/surveyCard/SurveyItem.js
+++ src/components/feedScreen/surveyCard/SurveyItem.js
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 
 const SurveyItem = ({ item, onPress, backgroundColor, textColor }) => (
